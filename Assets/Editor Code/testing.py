004a2da23681d6a0fcba742cb77018bbca32d064
Tom H <tom.holewijn@protonmail.com>
2024-10-17 14:22:39 +0200

    no message

--- Assets/Editor Code/testing.py
+++ Assets/Editor Code/testing.py
@@ -1,9 +1,32 @@
-
+# -*- coding: utf-8 -*-
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from absl.testing import absltest
 
 import google.generativeai as genai
-import os
+import pathlib
 
-genai.configure(api_key=os.environ["AIzaSyColxW4aPKMYYfLS8vB00vAKVzfSB2iL_g"])
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Write a story about a magic backpack.")
-print(response.text)
+media = pathlib.Path(__file__).parents[1] / "third_party"
+
+
+class UnitTests(absltest.TestCase):
+    def test_text_gen_text_only_prompt(self):
+        # [START text_gen_text_only_prompt]
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        response = model.generate_content("Write a story about a magic backpack.")
+        print(response.text)
+        # [END text_gen_text_only_prompt]
+
+if __name__ == "__main__":
+    absltest.main()
